f779297bbd69cbfab44072e0c9e242b3efb6088f
azhar1598 <mohammedazhar.1598@gmail.com>
2024-11-14 21:34:21 +0530

    fix

--- app/(dashboard)/categories/[id]/add/page.tsx
+++ app/(dashboard)/categories/[id]/add/page.tsx
@@ -1,19 +1,28 @@
 "use client";
 import { PageHeader } from "@/components/common/PageHeader";
-import React from "react";
-import MerchantForm from "./MerchantForm";
-import PageMainWrapper from "@/components/common/PageMainWrapper";
-import MainLayout from "@/components/common/MainLayout";
-import AddProductsForm from "./ProductForm";
-import { useQuery } from "@tanstack/react-query";
+import { useForm, zodResolver } from "@mantine/form";
+import { z } from "zod";
+import { useMutation, useQuery } from "@tanstack/react-query";
 import callApi from "@/services/apiService";
-import { useParams } from "next/navigation";
+import { useParams, useRouter } from "next/navigation";
 import { IconPlus } from "@tabler/icons-react";
-import { Button, Group, SimpleGrid, Stack, TextInput } from "@mantine/core";
-import Link from "next/link";
+import { Button, Flex, Stack, FileInput, TextInput } from "@mantine/core";
+import PageMainWrapper from "@/components/common/PageMainWrapper";
+import { usePageNotifications } from "@/lib/hooks/useNotifications";
+
+const formSchema = z.object({
+  categories: z.array(
+    z.object({
+      name: z.string().min(1, "Category name is required"),
+      image: z.any().optional(),
+    })
+  ),
+});
 
 function AddProductPage() {
   const { id } = useParams();
+  const router = useRouter();
+  const notification = usePageNotifications();
 
   const getStoreById = useQuery({
     queryKey: ["get-store-by-id"],
@@ -21,24 +30,80 @@ function AddProductPage() {
       const response = await callApi.get(`/v1/stores/${id}`);
       return response.data;
     },
-    select: (data) => {
-      // Update form with fetched data
-      console.log("ddd", data.data);
-      return data;
+    select: (data) => data,
+  });
+
+  const form = useForm({
+    validate: zodResolver(formSchema),
+    initialValues: {
+      categories: [{ name: "", image: null }],
     },
   });
+
+  const handleAddCategory = () => {
+    form.setFieldValue("categories", [
+      ...form.values.categories,
+      { name: "", image: null },
+    ]);
+  };
+
+  const createCategory = useMutation({
+    mutationFn: () => callApi.post(`/v1/merchants`, form.values),
+    onSuccess: async (res) => {
+      const { data } = res;
+      console.log("res", data);
+      router.push(`/products/${id}/add`);
+      notification.success(`Category created successfully`);
+    },
+    onError: (err: Error) => {
+      console.log("ee", err);
+      notification.error(`${err}`);
+      console.log(err.message);
+    },
+  });
+
   return (
     <>
       <PageHeader
         title={`Create Category: ${getStoreById?.data?.data?.name}`}
       />
       <PageMainWrapper>
-        <Stack>
-          <Group>
-            <TextInput label="Category" w={"70%"} />
-          </Group>
-          <Button w={200}>Create</Button>
-        </Stack>
+        <form
+          onSubmit={() => {
+            createCategory.mutate();
+          }}
+        >
+          {form.values.categories.map((_, index) => (
+            <Stack key={index} mb={20}>
+              <Flex gap={10}>
+                <TextInput
+                  label="Category Name"
+                  {...form.getInputProps(`categories.${index}.name`)}
+                  w={300}
+                />
+                <FileInput
+                  label="Category Image"
+                  {...form.getInputProps(`categories.${index}.image`)}
+                  w={300}
+                />
+                {form.values.categories.length === index + 1 && (
+                  <Button
+                    leftSection={<IconPlus />}
+                    mt={24}
+                    onClick={handleAddCategory}
+                    w={200}
+                  >
+                    Add Category
+                  </Button>
+                )}
+              </Flex>
+            </Stack>
+          ))}
+
+          <Button type="submit" w={200} loading={createCategory.isPending}>
+            Create
+          </Button>
+        </form>
       </PageMainWrapper>
     </>
   );
