bcbea3015ce1573c0a4bdcde4127b7d4db7c0ba9
azhar1598 <mohammedazhar.1598@gmail.com>
2025-07-14 02:44:05 +0530

    sidebarr dsign

--- components/common/PageMainWrapper/index.tsx
+++ components/common/PageMainWrapper/index.tsx
@@ -1,10 +1,22 @@
+import { Box } from "@mantine/core";
 import React from "react";
 
 function PageMainWrapper({ children, w = "1/2" }) {
   return (
-    <div className="bg-white w-full mt-5 page-main-wrapper shadow-xl p-[20px]">
-      <div className={`md:w-${[w]} w-full`}>{children}</div>
-    </div>
+    <Box
+      mt="lg"
+      style={{
+        background:
+          "linear-gradient(to bottom, rgba(255,255,255,0.8) 0%, rgba(255,255,255,0) 15%), #ffffff",
+        border: "1px solid rgba(0,0,0,0.1)",
+        borderRadius: "var(--mantine-radius-lg)",
+        boxShadow:
+          "0 8px 20px rgba(0, 0, 0, 0.07), 0 2px 6px rgba(0, 0, 0, 0.05)",
+        padding: "var(--mantine-spacing-xl)",
+      }}
+    >
+      <div className={`md:w-${w} w-full`}>{children}</div>
+    </Box>
   );
 }
 
